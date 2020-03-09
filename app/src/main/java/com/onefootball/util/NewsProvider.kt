57067bcc1a5bcb5f0b89c7539c27c584965f314b
Alex <xeal@Alex-MacBook-Pro.local>
2020-03-10 00:43:58 +0100

    ONF-309. Add exception handling for reading and parsing the file

--- app/src/main/java/com/onefootball/util/NewsProvider.kt
+++ app/src/main/java/com/onefootball/util/NewsProvider.kt
@@ -1,38 +1,53 @@
 package com.onefootball.util
 
 import android.content.res.AssetManager
+import android.util.Log
 import com.onefootball.model.News
+import org.json.JSONException
 import org.json.JSONObject
+import java.io.IOException
 import java.nio.charset.Charset
 
 object NewsProvider {
-    fun provideNewsFromFile(assetManager: AssetManager, fileName: String = DEFAULT_FILE_NAME): List<News> {
+    fun provideNewsFromFile(
+        assetManager: AssetManager,
+        fileName: String = DEFAULT_FILE_NAME
+    ) = try {
         val inputStream = assetManager.open(fileName)
         val size = inputStream.available()
         val buffer = ByteArray(size)
         inputStream.read(buffer)
         inputStream.close()
         val jsonString = buffer.toString(Charset.defaultCharset())
-        return parseJsonString(jsonString)
+        parseJsonString(jsonString)
+    } catch (e: IOException) {
+        Log.e(NewsProvider::class.java.toString(), e.message ?: "Unable to read the file")
+        listOf<News>()
     }
 
-    private fun parseJsonString(jsonString: String): List<News> {
-        val mainObject = JSONObject(jsonString)
-        val newsItems = mutableListOf<News>()
-        val newsArray = mainObject.getJSONArray(JSON_NEWS)
-        newsArray.forEach { newsObject ->
-            newsItems.add(
-                News(
-                    title = newsObject.getStringOrNull(JSON_TITLE),
-                    imageUri = newsObject.getStringOrNull(JSON_IMAGE_URL),
-                    resourceName = newsObject.getStringOrNull(JSON_RESOURCE_NAME),
-                    resourceUrl = newsObject.getStringOrNull(JSON_RESOURCE_URL),
-                    newsLink = newsObject.getStringOrNull(JSON_NEWS_LINK)
+
+    private fun parseJsonString(jsonString: String) =
+        try {
+            val mainObject = JSONObject(jsonString)
+            val newsItems = mutableListOf<News>()
+            val newsArray = mainObject.getJSONArray(JSON_NEWS)
+            newsArray.forEach { newsObject ->
+                newsItems.add(
+                    News(
+                        title = newsObject.getStringOrNull(JSON_TITLE),
+                        imageUri = newsObject.getStringOrNull(JSON_IMAGE_URL),
+                        resourceName = newsObject.getStringOrNull(JSON_RESOURCE_NAME),
+                        resourceUrl = newsObject.getStringOrNull(JSON_RESOURCE_URL),
+                        newsLink = newsObject.getStringOrNull(JSON_NEWS_LINK)
+                    )
                 )
-            )
+            }
+            newsItems
+        } catch (e: JSONException) {
+            Log.e(NewsProvider::class.java.toString(), e.message ?: "Unable to parse the file")
+            listOf<News>()
         }
-        return newsItems
-    }
+
 
     private const val DEFAULT_FILE_NAME = "news.json"
     private const val JSON_NEWS = "news"
